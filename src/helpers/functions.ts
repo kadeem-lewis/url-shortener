03805e6da739c15cfc0a51cb082b4538ede0c05d
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-12 19:20:46 -0400

    updated logic of function to check for unique urls

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -3,18 +3,24 @@ import { pb } from "../db/pocketbase.js";
 
 export async function generateUniqueShortUrl() {
   let unique = false;
-  let shortUrl;
+  let shortUrl: any;
 
   while (!unique) {
+    //generate a new uuid based on nanoid
     shortUrl = nanoid(7);
-    const existingRecord = await pb
-      .collection("urls")
-      .getFirstListItem(`shortened="http://short.est/${shortUrl}"`);
+    //check if a shortened url already exists with this id
+    const records = await pb.collection("urls").getFullList({
+      sort: "-created",
+    });
+    const existingRecord = records.find(
+      (record) => record.shortened === `http://short.est/${shortUrl}`
+    );
 
+    //if no record exists with this id then stop the loop and keep id
     if (!existingRecord) {
       unique = true;
     }
   }
-
+  //return the shortened url
   return `http://short.est/${shortUrl}`;
 }
