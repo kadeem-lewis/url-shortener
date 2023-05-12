4b2c865ceb32b7ed453c6af722bbea2c264593c5
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-12 19:21:35 -0400

    added logic to endpoints for decoding and encoding

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,63 @@ import http from "http";
 
 import express from "express";
 
-const app = express();
+import { Record } from "pocketbase";
+import { pb } from "./db/pocketbase.js";
+import { generateUniqueShortUrl } from "./helpers/functions.js";
 
+const app = express();
+//create a ejs view for app
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
 
+//allow app to use express json middleware
+app.use(express.json());
+
+//serving ejs view on "/"
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.post("/encode", (req, res) => {});
-app.post("/decode", (req, res) => {});
+
+app.post("/encode", async (req, res) => {
+  //get a list of the records and check to see if the record exists within list
+  const records = await pb.collection("urls").getFullList({
+    sort: "-created",
+  });
+  const record = records.find((record) => record.original === req.body.url);
+  console.log(records);
+  if (record) {
+    //if the record exists than just return the saved shortened url from the data
+    res.json({ shortened: record?.shortened });
+  } else {
+    //if record doesnt exist generate a new shortened url
+    const shortUrl = await generateUniqueShortUrl();
+    const data = {
+      original: req.body.url,
+      shortened: shortUrl,
+    };
+    try {
+      //add the original url and the shortened url to the database
+      const newRecord = await pb.collection("urls").create(data);
+      //return the shortened url to client as a json object
+      res.json({ shortened: data.shortened });
+    } catch (error) {
+      res.json({ response: "Creating shortened URL failed" });
+    }
+  }
+});
+
+app.post("/decode", async (req, res) => {
+  try {
+    const record = await pb
+      .collection("urls")
+      .getFirstListItem(`shortened="${req.body.url}"`);
+    res.json({ response: record.original });
+  } catch (error) {
+    res.json({ response: "Invalid URL" });
+  }
+});
+
+//creating server
 const server = http.createServer(app);
 const PORT = 3005;
 server.listen(PORT, () => {
