8e10ad7aaec9c47bdd2bd4f1664dcfeb4e80e405
kadeem-lewis <print.helloworld@genericindividual.com>
2023-05-12 19:28:05 -0400

    updated pocketbase config to use envs instead of hard coded values

--- src/db/pocketbase.ts
+++ src/db/pocketbase.ts
@@ -1,7 +1,8 @@
 import PocketBase from "pocketbase";
-
+import * as dotenv from "dotenv";
+dotenv.config();
+//
 export const pb = new PocketBase("http://127.0.0.1:8090");
-const authData = await pb.admins.authWithPassword(
-  "email@kadeemlewis.com",
-  "JH7wd8VX5ShY3BhD6h*#A6%LQ"
-);
+const USERNAME = process.env.DB_USERNAME as string;
+const PASSWORD = process.env.DB_PASSWORD as string;
+const authData = await pb.admins.authWithPassword(USERNAME, PASSWORD);
